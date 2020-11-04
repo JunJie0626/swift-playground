c98c34fb91e25f6f0b3d402d02c8bf3bc39d5a0e
杨俊杰 <y84104219@sina.com>
2020-11-04 17:03:31 +0800

    playground文件提交

---
+++ swift-01.playground/Contents.swift
@@ -0,0 +1,65 @@
+//import UIKit
+//
+//var str = "Hello, playground"
+////
+////var btn = UIButton()
+////
+////btn.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+////
+////btn.backgroundColor = UIColor.green
+////
+//
+//var str1 = "123"
+//var str2 = "3332"
+//var str3 = str1 + str2
+//
+//var str4 = "pinjie \(str1),pinjie2\(str2)"
+//
+//
+//
+//for i in 0..<6 {
+//    print(i)
+//}
+//
+//
+//let x = uint_fast8_t.max
+//
+//let y = x &+ 1
+//
+////var (x , y) = (10 , 20)
+//
+//var point = (10,20)
+//
+//var person = (name:"jack" , age:10)
+//
+//var i = 1
+//
+//
+//let name = ["11","22","33","44"]
+//
+//for name in name[2...] {
+//    print(name)
+//}
+//
+//
+//print(person.name,person.1)
+
+
+let range1 = "aa"..."zz"
+
+range1.contains("ab")
+
+let hours = 11
+let hourInterval = 3
+
+for tickMark in stride(from: 2, through: hours, by: hourInterval) {
+    print(tickMark)
+}
+
+
+for tickMark in stride(from: 2, to: hours, by: hourInterval) {
+    print(tickMark)
+}
+
+
+
